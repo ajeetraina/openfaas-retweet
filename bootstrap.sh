d273c55b0841dc783b4647b8bccc12be42685c3b
Ajeet Singh Raina, Docker Captain, {Code} Catalysts, Dell EMC R&D <ajeetraina@gmail.com>
2018-04-26 13:45:52 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# Setting up Visualizer
+docker-compose up -d
 git clone https://github.com/openfaas/faas
 cd faas
+
 ./deploy_stack.sh
 curl -sSL https://cli.openfaas.com | sudo sh
 mkdir -p ~/retweet && \
